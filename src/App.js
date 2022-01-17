768069943e6ede350c9c9c4abb7e4a3f5ac4aad0
unknown <katzelen88@yandex.ru>
2022-01-17 09:55:45 +0300

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
+import {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [style, setStyle] = useState('');
+  let [clearText, setClearText] = useState(false);
+
+  const onClearText = e => {
+    if(e.keyCode === 32){
+      setClearText(true);
+    }  
+  };
+
+  const onTransparentText = () => setStyle('opacity');
+
+  useEffect(() => {
+    document.addEventListener('keydown', e => onClearText(e));
+
+    return () => document.removeEventListener('keydown', onClearText);
+
+  }, [clearText]);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', () => onTransparentText());
+
+    return () => document.removeEventListener('mousedown', onTransparentText);
+
+  }, [style]);
+ 
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <div className={style} onMouseOver={() => setStyle('decoreted')} onMouseOut={() => setStyle('')} hidden={clearText}>
+             Тимофеева Екатерина Владимировна - Начинающий web-программист / Junior Frontend Developer       
+          </div>
+          {clearText && <button className="btn btn-dark" onClick={() => setClearText(false)}>Вернуть текст</button>}
       </header>
     </div>
   );
